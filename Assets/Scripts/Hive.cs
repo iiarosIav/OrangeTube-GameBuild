1b33b55e592d02ad864742ae4c66f4c10424bd87
blackstone1598 <Samuylo19@gmail.com>
2024-11-17 17:45:20 +0300

    Update Hive.cs

--- Assets/Scripts/Hive.cs
+++ Assets/Scripts/Hive.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HiveScript : InteractiveObject
+public class Hive : InteractiveObject
 {
     private float _honeyLevel; // кол-во меда в улье
     [SerializeField] private float _beeLevel; // уровень пчел в улье
